d5f1be4bec3045f79e288721186f81d4772b12df
MarianoDMartin <mariano.d.martin@gmail.com>
2019-06-27 00:33:53 -0300

    Agregar modelos y ejemplo de implementacion de controlador con vistas

---
+++ Presupuestador/Presupuestador/Controllers/ProyectosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Presupuestador.Models;
+
+namespace Presupuestador.Controllers
+{
+    public class ProyectosController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Proyectos
+        public ActionResult Index()
+        {
+            var proyectos = db.Proyectos.Include(p => p.Cliente);
+            return View(proyectos.ToList());
+        }
+
+        // GET: Proyectos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyecto proyecto = db.Proyectos.Find(id);
+            if (proyecto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyecto);
+        }
+
+        // GET: Proyectos/Create
+        public ActionResult Create()
+        {
+            ViewBag.cliente_id = new SelectList(db.Clientes, "id", "nombre");
+            return View();
+        }
+
+        // POST: Proyectos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,descripcion,cliente_id")] Proyecto proyecto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Proyectos.Add(proyecto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.cliente_id = new SelectList(db.Clientes, "id", "nombre", proyecto.cliente_id);
+            return View(proyecto);
+        }
+
+        // GET: Proyectos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyecto proyecto = db.Proyectos.Find(id);
+            if (proyecto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.cliente_id = new SelectList(db.Clientes, "id", "nombre", proyecto.cliente_id);
+            return View(proyecto);
+        }
+
+        // POST: Proyectos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,descripcion,cliente_id")] Proyecto proyecto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proyecto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.cliente_id = new SelectList(db.Clientes, "id", "nombre", proyecto.cliente_id);
+            return View(proyecto);
+        }
+
+        // GET: Proyectos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyecto proyecto = db.Proyectos.Find(id);
+            if (proyecto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyecto);
+        }
+
+        // POST: Proyectos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Proyecto proyecto = db.Proyectos.Find(id);
+            db.Proyectos.Remove(proyecto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
