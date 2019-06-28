5ae3ddea36017f8b0a9f0a6366ec246d6e0c7ecb
gferreira3 <f.gab@hotmail.com>
2019-06-28 01:10:53 -0300

    Se eliminan archivos innecesarios y se agregan estilos bootstrap

--- Presupuestador/Presupuestador/App_Start/RouteConfig.cs
+++ Presupuestador/Presupuestador/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Presupuestador
       routes.MapRoute(
           name: "Default",
           url: "{controller}/{action}/{id}",
-          defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+          defaults: new { controller = "Proyectos", action = "Index", id = UrlParameter.Optional }
       );
     }
   }
