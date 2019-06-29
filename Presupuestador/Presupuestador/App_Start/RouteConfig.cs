a6462cdd096be34eae7fd727ee5a198509227a3b
gferreira3 <f.gab@hotmail.com>
2019-06-29 17:20:37 -0300

    Se empieza funcionalidad de presupuesto

--- Presupuestador/Presupuestador/App_Start/RouteConfig.cs
+++ Presupuestador/Presupuestador/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Presupuestador
       routes.MapRoute(
           name: "Default",
           url: "{controller}/{action}/{id}",
-          defaults: new { controller = "Proyectos", action = "Index", id = UrlParameter.Optional }
+          defaults: new { controller = "Presupuestos", action = "Index", id = UrlParameter.Optional }
       );
     }
   }
