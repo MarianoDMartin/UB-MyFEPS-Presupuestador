a55b57152d3a73b1c2284eb15ebc73b422b6f1b8
MarianoDMartin <mariano.d.martin@gmail.com>
2019-06-30 01:10:32 -0300

    Agregar abm de categorias y algunos fixes sobre proyectos

---
+++ Presupuestador/Presupuestador/Controllers/CategoriasController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Presupuestador.Models;
+
+namespace Presupuestador.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private Entities1 db = new Entities1();
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            var categorias = db.Categorias.Include(c => c.Categoria1).ToList().Where(c => c.Categoria1 == null);
+            
+            return View(categorias.ToList());
+        }
+
+        // GET: Categorias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = db.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        // GET: Categorias/Create
+        public ActionResult Create()
+        {
+            ViewBag.categoria_padre = new SelectList(db.Categorias, "id", "descripcion");
+            return View();
+        }
+
+        // POST: Categorias/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,descripcion,categoria_padre")] Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categorias.Add(categoria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.categoria_padre = new SelectList(db.Categorias, "id", "descripcion", categoria.categoria_padre);
+            return View(categoria);
+        }
+
+        // GET: Categorias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = db.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.categoria_padre = new SelectList(db.Categorias.Where(c => c.id != id), "id", "descripcion", categoria.categoria_padre);
+            return View(categoria);
+        }
+
+        // POST: Categorias/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,descripcion,categoria_padre")] Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(categoria).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.categoria_padre = new SelectList(db.Categorias, "id", "descripcion", categoria.categoria_padre);
+            return View(categoria);
+        }
+
+        // GET: Categorias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = db.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        // POST: Categorias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Categoria categoria = db.Categorias.Find(id);
+            db.Categorias.Remove(categoria);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
