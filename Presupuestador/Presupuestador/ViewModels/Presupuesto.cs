0873221fc8710ff18e1b49076fc8feebfa2c30a1
gferreira3 <f.gab@hotmail.com>
2019-07-04 19:23:05 -0300

    Se actualiza creación de presupuesto

--- Presupuestador/Presupuestador/ViewModels/Presupuesto.cs
+++ Presupuestador/Presupuestador/ViewModels/Presupuesto.cs
@@ -1,7 +1,25 @@
-﻿namespace Presupuestador.ViewModels
+﻿using System;
+using System.Collections.Generic;
+
+namespace Presupuestador.ViewModels
 {
   public class Presupuesto
   {
-
+    public int id { get; set; }
+    public string descripcion { get; set; }
+    public int ciclos_test { get; set; }
+    public int tiempo_test { get; set; }
+    public DateTime fecha_creacion { get; set; }
+    public DateTime fecha_vencimiento { get; set; }
+    public double? cargas_sociales { get; set; }
+    public double? markup { get; set; }
+    public double? costo_base { get; set; }
+    public string creador { get; set; }
+    public int proyecto_id { get; set; }
+    public int estado_id { get; set; }
+    public int Tareas { get; set; }
+    public int Recursos { get; set; }
+    public int valorHora { get; set; }
+    public List<Tarea> TareasAsignadas { get; set; } = new List<Tarea>();
   }
 }
