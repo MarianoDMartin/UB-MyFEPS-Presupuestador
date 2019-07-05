721983565bfb8aee46c7020878c24e62cef4fef6
gferreira3 <f.gab@hotmail.com>
2019-07-04 23:04:44 -0300

    CRUD Presupuesto

--- Presupuestador/Presupuestador/ViewModels/PresupuestoViewModel.cs
+++ Presupuestador/Presupuestador/ViewModels/PresupuestoViewModel.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 
 namespace Presupuestador.ViewModels
 {
-  public class Presupuesto
+  public class PresupuestoViewModel
   {
     public int id { get; set; }
     public string descripcion { get; set; }
     public int ciclos_test { get; set; }
     public int tiempo_test { get; set; }
     public DateTime fecha_creacion { get; set; }
-    public DateTime fecha_vencimiento { get; set; }
+    public DateTime? fecha_vencimiento { get; set; }
     public double? cargas_sociales { get; set; }
     public double? markup { get; set; }
     public double? costo_base { get; set; }
@@ -20,6 +20,6 @@ namespace Presupuestador.ViewModels
     public int Tareas { get; set; }
     public int Recursos { get; set; }
     public int valorHora { get; set; }
-    public List<Tarea> TareasAsignadas { get; set; } = new List<Tarea>();
+    public List<TareaViewModel> TareasAsignadas { get; set; } = new List<TareaViewModel>();
   }
 }
