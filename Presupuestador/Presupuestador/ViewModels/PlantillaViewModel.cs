9a1e64fa059f5e7387d2719b7bee814db7dbea1e
MarianoDMartin <mariano.d.martin@gmail.com>
2019-07-05 01:21:22 -0300

    Termino create de plantillas con lista

---
+++ Presupuestador/Presupuestador/ViewModels/PlantillaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Presupuestador.ViewModels
+{
+  public class PlantillaViewModel
+  {
+    public int id { get; set; }
+    public string nombre { get; set; }
+    public int tarea_id { get; set; }
+    public List<TareaViewModel> TareasAsignadas { get; set; } = new List<TareaViewModel>();
+  }
+}
