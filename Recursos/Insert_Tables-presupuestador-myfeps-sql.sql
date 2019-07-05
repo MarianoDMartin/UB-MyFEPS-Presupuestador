da5022e23871e867b0b54c471eda9506ef1dcd8d
nereocandenas <nereocandenas@gmail.com>
2019-07-05 14:20:10 -0300

    Actualizacion de los inserts

--- Recursos/Insert_Tables-presupuestador-myfeps-sql.sql
+++ Recursos/Insert_Tables-presupuestador-myfeps-sql.sql
@@ -20,6 +20,7 @@ select * from Tipos_Tareas
 -- Tablas Maestras
 
 -- Insert de presupuestos_estados
+insert into Presupuestos_Estados (descripcion) values ('Eliminado')
 insert into Presupuestos_Estados (descripcion) values ('Borrador')
 insert into Presupuestos_Estados (descripcion) values ('Aceptado')
 insert into Presupuestos_Estados (descripcion) values ('Rechazado')
@@ -56,102 +57,75 @@ insert into proyectos (nombre,descripcion,contacto_id) values ('Proyecto Banelco
 insert into proyectos (nombre,descripcion,contacto_id) values ('Proyecto Desarrollo CRM','Sistema CRM Web para empresas','2')
 insert into proyectos (nombre,descripcion,contacto_id) values ('Proyecto Seguridad Web','Soluciones de seguridad Web para empresas','1')
 
--- Insert de Categorias
-insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Juan Perez',1,1,100)
-insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Daniel Lopez',2,1,200)
-insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Martin Garcia',2,2,220)
-insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Juana Ortiz',3,3,300)
-insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Francisco J',4,3,400)
+-- Insert de Recursos
+insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Desarrollador - Junior',1,1,100)
+insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Desarrollador - SemiSenior',2,1,170)
+insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Desarrollador - Junior',3,1,250)
+insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Lider de Proyecto - SemiSenior',2,3,200)
+insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Lider de Proyecto - SemiSenior',3,3,320)
+insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Usuario Prueba - Junior',1,5,100)
+insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Usuario Prueba - SemiSenior',2,5,130)
+insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Usuario Prueba - Senior',3,5,180)
+insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Administrador DBs - Senior',3,4,400)
+insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Ing de Software - Senior',3,7,400)
+insert into Recursos (descripcion,rango_id,rol_id,valorHora) values ('Ing de Software - Master',4,7,600)
+
 
 --Categorias Padre
 insert into Categorias (descripcion) values ('Proceso de Evaluación')
---Sub Categoria Nivel 1
 insert into Categorias (descripcion,categoria_padre) values ('Establecer Requisitos y objetivos',1)
-insert into Categorias (descripcion,categoria_padre) values ('Especificar la Evaluación',1)
-insert into Categorias (descripcion,categoria_padre) values ('Diseñar la Evaluación',1)
-insert into Categorias (descripcion,categoria_padre) values ('Evaluar el Producto',1)
-insert into Categorias (descripcion,categoria_padre) values ('Concluir',1)
-
-select * from categorias
---Sub Categoria Nivel 2
---2	Establecer Requisitos y objetivos
---3	Especificar la Evaluación
---4	Diseñar la Evaluación
---5	Evaluar el Producto
---6	Concluir
-insert into Categorias (descripcion,categoria_padre) values ('Establecer el Propósito de la Evaluación',2)
-insert into Categorias (descripcion,categoria_padre) values ('Identificar el Producto a Evaluar',2)
-insert into Categorias (descripcion,categoria_padre) values ('Identificarlos Requerimientos de Calidad',2)
 insert into Categorias (descripcion,categoria_padre) values ('Elegir el Marco de Calidad',2)
 insert into Categorias (descripcion,categoria_padre) values ('Administrar Riesgos de la Evaluación',2)
-
-insert into Categorias (descripcion,categoria_padre) values ('Definir Recursos para la Evaluación',3)
-insert into Categorias (descripcion,categoria_padre) values ('Conformar Equipo Evaluador',3)
-insert into Categorias (descripcion,categoria_padre) values ('Seleccionar las Métricas',3)
-insert into Categorias (descripcion,categoria_padre) values ('Establecer Ponderación',3)
-insert into Categorias (descripcion,categoria_padre) values ('Establecer Fidelidades de Medición',3)
-
-insert into Categorias (descripcion,categoria_padre) values ('Preparar Recursos e Infraestructura',4)
-insert into Categorias (descripcion,categoria_padre) values ('Elaborar el Plan de Mediciones',4)
-insert into Categorias (descripcion,categoria_padre) values ('Elaborar las Pruebas',4)
-insert into Categorias (descripcion,categoria_padre) values ('Obtener y Adecuar Recursos para la Evaluación',4)
-
-insert into Categorias (descripcion,categoria_padre) values ('StakeHolders debe completar los Surveys',5)
-insert into Categorias (descripcion,categoria_padre) values ('Realizar las Pruebas',5)
-insert into Categorias (descripcion,categoria_padre) values ('Medir los Atributos requeridos por las Métricas',5)
-insert into Categorias (descripcion,categoria_padre) values ('Registrar los Valores de las Métricas en el Modelo de Evaluación',5)
-insert into Categorias (descripcion,categoria_padre) values ('Obtener Resultados',5)
-
-insert into Categorias (descripcion,categoria_padre) values ('Analizar los resultados ',6)
-insert into Categorias (descripcion,categoria_padre) values ('Concluir el Grado de Calidad',6)
-insert into Categorias (descripcion,categoria_padre) values ('Documentar la Evaluación ',6)
-
-
---Sub Categoria Nivel 3  Se cargan directamente como tareas
---insert into Categorias (descripcion,categoria_padre) values ('Definir El Modelo de Calidad a Usar',10)
---insert into Categorias (descripcion,categoria_padre) values ('Definir la Rigurosidad del Modelo',10)
---insert into Categorias (descripcion,categoria_padre) values ('Identificar Riesgos de Proyecto y/o Producto',11)
---insert into Categorias (descripcion,categoria_padre) values ('Analizar los Riesgos',11)
---insert into Categorias (descripcion,categoria_padre) values ('Tratar los Riesgos',11)
---insert into Categorias (descripcion,categoria_padre) values ('Monitorear los Riesgos',11)
---insert into Categorias (descripcion,categoria_padre) values ('Elaborar casos de Prueba',19)
---insert into Categorias (descripcion,categoria_padre) values ('Diseñar El Ambiente de Prueba',19)
---insert into Categorias (descripcion,categoria_padre) values ('Documentar los Casos de Prueba',19)
---insert into Categorias (descripcion,categoria_padre) values ('Armar el Ambiente de Prueba',22)
---insert into Categorias (descripcion,categoria_padre) values ('Ejecutar Casos de Prueba',22)
---insert into Categorias (descripcion,categoria_padre) values ('Documentar sus Ejecuciones de Pruebas',22)
---insert into Categorias (descripcion,categoria_padre) values ('Concluir la Prueba',22)
-
-
--- Recursos
-insert into recursos (descripcion,valorHora,rango_id,rol_id) values ('Marcelo Garcia',200,1,1)
-insert into recursos (descripcion,valorHora,rango_id,rol_id) values ('Marcelo Garcia',200,2,1)
-insert into recursos (descripcion,valorHora,rango_id,rol_id) values ('Juan Perez',280,3,1)
-insert into recursos (descripcion,valorHora,rango_id,rol_id) values ('Pablo Krat',310,3,1)
-insert into recursos (descripcion,valorHora,rango_id,rol_id) values ('Julia Melle',200,3,2)
-insert into recursos (descripcion,valorHora,rango_id,rol_id) values ('Andres Pila',400,3,4)
-insert into recursos (descripcion,valorHora,rango_id,rol_id) values ('Karina Gomez',420,3,3)
-insert into recursos (descripcion,valorHora,rango_id,rol_id) values ('Karina Gomez',220,3,2)
-insert into recursos (descripcion,valorHora,rango_id,rol_id) values ('Carlos Paz',520,4,7)
-
-select * from Tipos_Tareas
-select * from Categorias
+insert into Categorias (descripcion,categoria_padre) values ('Especificar la Evaluación',1)
+insert into Categorias (descripcion,categoria_padre) values ('Diseñar la Evaluación',1)
+insert into Categorias (descripcion,categoria_padre) values ('Elaborar las Pruebas',6)
+insert into Categorias (descripcion,categoria_padre) values ('Evaluar el Producto',1)
+insert into Categorias (descripcion,categoria_padre) values ('Realizar Pruebas',1)
+insert into Categorias (descripcion,categoria_padre) values ('Concluir',1)
 
 
 -- Tareas
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values('Definir El Modelo de Calidad a Usar','Realizar reuniones para definir El Modelo de Calidad a Usar',1,10)
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Definir la Rigurosidad del Modelo','Realizar reuniones para definir Rigurosidad del Modelo',1,10)
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Identificar Riesgos de Proyecto y/o Producto','Realizar relevamiento para Identificar Riesgos potenciales que afecten al proyecto/producto',1,11)
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Analizar los Riesgos','Realizar el analisis de los riesgos identificados',2,11)
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Tratar los Riesgos','Tratar los Riesgos identificados',2,11)
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Monitorear los Riesgos','Realizar el monitoreo de los riesgos identificados',3,11)
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Elaborar casos de Prueba','Realizar junto con el cliente, los casos de prueba a ejecutar',1,19)
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Diseñar El Ambiente de Prueba','Realizar las tareas necesarias para diseñar el ambiente de pruebas',3,19)
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Documentar los Casos de Prueba','Realziar la docuemtancion de los casos de pruebas ejecutados',1,19)
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Armar el Ambiente de Prueba','Realizar las tareas necesarias para armar el ambiente de pruebas',3,22)
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Ejecutar Casos de Prueba','Ejecutar los casos de pruebas diseñados en el ambiente de pruebas',3,22)
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Documentar sus Ejecuciones de Pruebas','Realizar la docuemntacion de los resultados de la sejecuciones de las pruebas',1,22)
-insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Concluir la Prueba','Elaborar la conclusion de las pruebas realizadas',2,22)
+
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Establecer el Propósito de la Evaluación',' Realizar reuniones para Establecer el Propósito de la Evaluación',1,2)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Identificar el Producto a Evaluar','Realizar relevamiento para Identificar el Producto a Evaluar',1,2)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Identificarlos Requerimientos de Calidad','Realizar relevamiento para Identificarlos Requerimientos de Calidad',1,2)
+
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Definir El Modelo de Calidad a Usar','Realizar reuniones para definir El Modelo de Calidad a Usar',1,3)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Definir la Rigurosidad del Modelo','Realizar reuniones para definir Rigurosidad del Modelo',1,3)
+
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Identificar Riesgos de Proyecto y/o Producto','Realizar relevamiento para Identificar Riesgos potenciales que afecten al proyecto/producto',1,4)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Analizar los Riesgos','Realizar el analisis de los riesgos identificados',2,4)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Tratar los Riesgos','Tratar los Riesgos identificados',2,4)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Monitorear los Riesgos','Realizar el monitoreo de los riesgos identificados',3,4)
+
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Definir Recursos para la Evaluación','Realizar reuniones para Definir Recursos para la Evaluación',1,5)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Conformar Equipo Evaluador','Realizar reuniones para Conformar Equipo Evaluador',1,5)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Seleccionar las Métricas','Realizar reuniones para Seleccionar las Métricas',1,5)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Establecer Ponderación','Realizar reuniones para Establecer Ponderación',1,5)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Establecer Fidelidades de Medición','Realizar reuniones para Establecer Fidelidades de Medición',1,5)
+
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Preparar Recursos e Infraestructura','Realizar reuniones para Preparar Recursos e Infraestructura',1,6)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Elaborar el Plan de Mediciones','Realizar reuniones para Elaborar el Plan de Mediciones',1,6)
+
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Elaborar casos de Prueba','Realizar junto con el cliente, los casos de prueba a ejecutar',1,7)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Diseñar El Ambiente de Prueba','Realizar las tareas necesarias para diseñar el ambiente de pruebas',3,7)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Documentar los Casos de Prueba','Realziar la docuemtancion de los casos de pruebas ejecutados',1,7)
+
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Obtener y Adecuar Recursos para la Evaluación','Realizar reuniones para Obtener y Adecuar Recursos para la Evaluación',1,7)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('StakeHolders debe completar los Surveys','Realizar reuniones para que StakeHolders complete los Surveys',1,8)
+
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Armar el Ambiente de Prueba','Realizar las tareas necesarias para armar el ambiente de pruebas',3,9)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Ejecutar Casos de Prueba','Ejecutar los casos de pruebas diseñados en el ambiente de pruebas',3,9)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Documentar sus Ejecuciones de Pruebas','Realizar la docuemntacion de los resultados de la sejecuciones de las pruebas',1,9)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Concluir la Prueba','Elaborar la conclusion de las pruebas realizadas',2,9)
+
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Medir los Atributos requeridos por las Métricas','Realizar las tareas necesarias para Medir los Atributos',1,8)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Registrar los Valores de las Métricas en el Modelo de Evaluación','Registrar los Valores de las Métricas en el Modelo de Evaluación',1,8)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Obtener Resultados','Realizar las tareas necesarias para Obtener Resultados',1,8)
+
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Analizar los resultados ','Realizar las tareas necesarias para Analizar los resultados',1,10)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Concluir el Grado de Calidad','Realizar las tareas necesarias para Concluir el Grado de Calidad',1,10)
+insert into Tareas (titulo,descripcion,tipo_tarea_id,categoria_id) values ('Documentar la Evaluación ','Realizar las tareas necesarias para Documentar la Evaluación',1,10)
 
 
 
